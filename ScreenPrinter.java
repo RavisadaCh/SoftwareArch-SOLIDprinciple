a35aacb487d295246ccbd715f9112e98fb399ed8
RavisadaCh <69179017+RavisadaCh@users.noreply.github.com>
2022-10-19 22:31:20 +0700

    Add files via upload

---
+++ ScreenPrinter.java
@@ -0,0 +1,11 @@
+//import javax.print.StreamPrintService;
+
+public class ScreenPrinter implements Printer {
+    public void print(Book book){
+        do{
+            System.out.println(book.getCurrentPage());
+        }
+        while(book.turnToNextPage());
+    }
+}
+
